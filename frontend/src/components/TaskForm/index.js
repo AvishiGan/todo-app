13d14354e583b90e722bb3597fbd8f8ec3b0c22d
AvishiGan <avishiganepola@gmail.com>
2025-02-27 16:27:08 +0530

    Clean up code

--- frontend/src/components/TaskForm/index.js
+++ frontend/src/components/TaskForm/index.js
@@ -11,7 +11,7 @@ const TaskForm = ({ onTaskCreated }) => {
     e.preventDefault();
     if (title.trim() === "") return;
 
-    setIsLoading(true); // Start loading
+    setIsLoading(true);
     try {
       await onTaskCreated({ title, description });
       setTitle("");
@@ -19,7 +19,7 @@ const TaskForm = ({ onTaskCreated }) => {
     } catch (error) {
       console.error("Error creating task:", error);
     } finally {
-      setIsLoading(false); // Stop loading
+      setIsLoading(false);
     }
   };
 
